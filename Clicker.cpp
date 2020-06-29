ed11cfc80f6f0782780aab0eb6707998fe133066
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-06-29 17:23:22 +0200

    formatting

--- Clicker.cpp
+++ Clicker.cpp
@@ -12,6 +12,32 @@ POINT Clicker::get_cursor_pos() {
     return point;
 }
 
+POINT Clicker::scan_area(int x_min, int x_max, int y_min, int y_max, const char* color, int debug) {
+    POINT point = {};
+    char* hex_color = new char[Clicker::buffer];
+
+    for (int i = x_min; i <= x_max; i++) {
+        for (int j = y_min; j <= y_max; j++) {
+            hex_color = Clicker::get_hex_color(i, j);
+            if (strcmp(hex_color, color) == 0) {
+                delete[] hex_color;
+                point.x = i;
+                point.y = j;
+                return point;
+            }
+        }
+    }
+    delete[] hex_color;
+    return point;
+}
+
+POINT Clicker::rect_to_point(RECT rect) {
+    POINT point = {};
+    point.x = rect.right - rect.left;
+    point.y = rect.bottom - rect.top;
+    return point;
+}
+
 COLORREF Clicker::get_pixel_screen(int x, int y) {
     HDC hdc = GetWindowDC(NULL);
     COLORREF colorref = GetPixel(hdc, x, y);
@@ -40,6 +66,11 @@ char* Clicker::get_window_name() {
     return window_name;
 }
 
+char* Clicker::get_hex_color(int x, int y) {
+    COLORREF colorref = Clicker::get_pixel_screen(x, y);
+    return colorref_to_color(colorref);
+}
+
 void Clicker::click_release_left() {
     INPUT* input = new INPUT;
     input->type = INPUT_MOUSE;
@@ -62,9 +93,20 @@ void Clicker::move_and_click(int x, int y, unsigned int restore) {
     }
 }
 
-char* Clicker::get_hex_color(int x, int y) {
-    COLORREF colorref = Clicker::get_pixel_screen(x, y);
-    return colorref_to_color(colorref);
+void Clicker::client_to_screen(POINT* point) {
+    HWND hwnd = GetForegroundWindow();
+    ClientToScreen(hwnd, point);
+}
+
+void Clicker::log_mouse(int timeout) {
+    clock_t start = clock();
+    POINT point;
+    char* hex_color = new char[8];
+    while (Clicker::check_timeout(start, timeout)) {
+        point = Clicker::get_cursor_pos();
+        hex_color = Clicker::get_hex_color(point.x, point.y);
+        printf("(%i, %i): %s\n", point.x, point.y, hex_color);
+    }
 }
 
 int Clicker::wait_for_pixel(int x, int y, const char* color, int timeout, int sleep_ms) {
@@ -94,44 +136,6 @@ int Clicker::wait_and_click(int x, int y, const char* color, int timeout, int sl
     return i;
 }
 
-void Clicker::client_to_screen(POINT* point) {
-    HWND hwnd = GetForegroundWindow();
-    ClientToScreen(hwnd, point);
-}
-
-POINT Clicker::scan_area(int x_min, int x_max, int y_min, int y_max, const char* color, int debug) {
-    POINT point = {};
-    char* hex_color = new char[Clicker::buffer];
-
-    for (int i = x_min; i <= x_max; i++) {
-        for (int j = y_min; j <= y_max; j++) {
-            hex_color = Clicker::get_hex_color(i, j);
-            if (strcmp(hex_color, color) == 0) {
-                delete[] hex_color;
-                point.x = i;
-                point.y = j;
-                return point;
-            }
-        }
-    }
-    delete[] hex_color;
-    return point;
-}
-
-RECT Clicker::get_window_rect_foreground() {
-    RECT rect;
-    HWND hwnd = GetForegroundWindow();
-    GetWindowRect(hwnd, &rect);
-    return rect;
-}
-
-POINT Clicker::rect_to_point(RECT rect) {
-    POINT point = {};
-    point.x = rect.right - rect.left;
-    point.y = rect.bottom - rect.top;
-    return point;
-}
-
 int Clicker::check_rect_bounds(RECT rect, POINT point) {
     POINT rect_size = Clicker::rect_to_point(rect); 
     int in_bounds = 0;
@@ -149,15 +153,11 @@ int Clicker::check_timeout(clock_t start, int timeout) {
     return timeouted;
 }
 
-void Clicker::log_mouse(int timeout) {
-    clock_t start = clock();
-    POINT point;
-    char* hex_color = new char[8];
-    while (Clicker::check_timeout(start, timeout)) {
-        point = Clicker::get_cursor_pos();
-        hex_color = Clicker::get_hex_color(point.x, point.y);
-        printf("(%i, %i): %s\n", point.x, point.y, hex_color);
-    }
+RECT Clicker::get_window_rect_foreground() {
+    RECT rect;
+    HWND hwnd = GetForegroundWindow();
+    GetWindowRect(hwnd, &rect);
+    return rect;
 }
 
 void Clicker::log_area(int x_min, int x_max, int y_min, int y_max, const char* file_name) {
--- Clicker.h
+++ Clicker.h
@@ -17,21 +17,27 @@ public:
     static const unsigned int buffer;
 
     static POINT get_cursor_pos();
+    static POINT scan_area(int x_min, int x_max, int y_min, int y_max, const char* color, int debug = 1);
+    static POINT rect_to_point(RECT rect);
+
     static COLORREF get_pixel_screen(int x, int y);
     static COLORREF get_pixel_foreground(int x, int y);
+
     static char* colorref_to_color(COLORREF colorref);
     static char* get_window_name();
+    static char* get_hex_color(int x, int y);
+
     static void click_release_left();
     static void move_and_click(int x, int y, unsigned int restore = 1);
-    static char* get_hex_color(int x, int y);
+    static void client_to_screen(POINT* point);
+    static void log_mouse(int timeout);
+
     static int wait_for_pixel(int x, int y, const char* color, int timeout = 0, int sleep_ms = 1000);
     static int wait_and_click(int x, int y, const char* color, int timeout = 0, int sleep_ms = 1000);
-    static POINT scan_area(int x_min, int x_max, int y_min, int y_max, const char* color, int debug = 1);
-    static RECT get_window_rect_foreground();
-    static POINT rect_to_point(RECT rect);
-    static void client_to_screen(POINT* point);
     static int check_rect_bounds(RECT rect, POINT point);
     static int check_timeout(clock_t start, int timeout);
-    static void log_mouse(int timeout);
+
+    static RECT get_window_rect_foreground();
+
     void log_area(int x_min, int x_max, int y_min, int y_max, const char* file_name = "logs.txt");
 };
